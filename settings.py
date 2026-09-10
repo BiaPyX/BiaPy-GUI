2bfc273319b21d4c2cbfbcf76db68c16d797b2b9
Daniel Franco <danialtza@gmail.com>
2026-09-10 12:43:30 +0000

    Move release

--- settings.py
+++ settings.py
@@ -31,9 +31,9 @@ class Settings:
         self.settings["CUDA_selected"] = "" # to be filled by checking the GPU 
 
         # BiaPy
-        self.settings["biapy_code_version"] = Version("v3.6.8")
+        self.settings["biapy_code_version"] = Version("v3.7.1")
         self.settings["biapy_code_github"] = "https://github.com/BiaPyX/BiaPy"
-        self.settings["biapy_gui_version"] = Version("v1.2.3")
+        self.settings["biapy_gui_version"] = Version("v1.2.4")
         self.settings["biapy_gui_github"] = "https://github.com/BiaPyX/BiaPy-GUI"
         self.settings["biapy_container_basename"] = "biapyx/biapy"
         self.settings["biapy_container_name"] = (
